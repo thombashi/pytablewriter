30986553a62abafcb465ae38cc729831b63c5d33
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-05-30 22:00:26 +0900

    Modify: strip double quote is no longer needed

--- pytablewriter/_javascript_writer.py
+++ pytablewriter/_javascript_writer.py
@@ -27,7 +27,6 @@ class JavaScriptTableWriter(SourceCodeTableWriter):
         super(JavaScriptTableWriter, self).__init__()
 
         self._prop_extractor.none_value = "null"
-        self.__re_replace_null = re.compile('["]null["]', re.MULTILINE)
 
     def write_table(self):
         """
@@ -44,8 +43,6 @@ class JavaScriptTableWriter(SourceCodeTableWriter):
         super(JavaScriptTableWriter, self).write_table()
         self.dec_indent_level()
         data_frame_text = self.stream.getvalue().rstrip(u",\n")
-        data_frame_text = self.__re_replace_null.sub(
-            self._prop_extractor.none_value, data_frame_text)
 
         self.stream.close()
         self.stream = org_stream
