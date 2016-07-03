c6da077433dfb13e80bc649dea234d7975e5aaad
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-03 10:52:03 +0900

    Initial commit

---
+++ pytablewriter/_converter.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+
+def lower_bool_converter(bool_value):
+    return str(bool_value).lower()
