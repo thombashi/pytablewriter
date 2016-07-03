d2f515d04c25d7a875993148db50202fcdfb73df
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-03 16:28:05 +0900

    Add strip_quote function

--- pytablewriter/_converter.py
+++ pytablewriter/_converter.py
@@ -5,7 +5,14 @@
 """
 
 from __future__ import absolute_import
+import re
 
 
 def lower_bool_converter(bool_value):
     return str(bool_value).lower()
+
+
+def strip_quote(text, value):
+    re_replace = re.compile(
+        '["\']%s["\']' % (value), re.MULTILINE)
+    return re_replace.sub(value, text)
