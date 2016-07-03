a0b630c82d11cc41589638c56b5335465ffc4a88
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-03 10:04:11 +0900

    Add test dataset for mixed type values

--- test/data.py
+++ test/data.py
@@ -6,6 +6,9 @@
 
 from __future__ import absolute_import
 
+_inf = float("inf")
+_nan = float("nan")
+
 header_list = ["a", "b", "c", "dd", "e"]
 value_matrix = [
     ["1", 123.1, "a", "1",   1],
@@ -17,4 +20,11 @@ value_matrix_with_none = [
     [None, 2.2, None, "2.2", 2.2],
     [3, 3.3, "ccc", None,   "cccc"],
     [None, None, None, None,   None],
+]
+
+mix_header_list = ["i", "f", "c", "if", "ifc", "bool", "inf", "nan", "mix"]
+mix_value_matrix = [
+    [1, 1.1,  "aa",   1,   1,     True,   _inf,       _nan,  1.0],
+    [2, 2.2,  "bbb",  2.2, 2.2,   False,  "inf",      "nan", _inf],
+    [3, 3.33, "cccc", -3,  "ccc", "true", "infinity", "NAN", _nan],
 ]
