7ae77435339023261311f60f088b2165307a4b43
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-16 23:32:50 +0900

    Update example

--- examples/py/excel_table_single.py
+++ examples/py/excel_table_single.py
@@ -4,17 +4,26 @@
 import pytablewriter
 
 
-writer = pytablewriter.ExcelXlsxTableWriter()
-writer.open_workbook("sample.xlsx")
-
-writer.make_worksheet("example")
-writer.header_list = ["int", "float", "str", "bool", "mix", "time"]
-writer.value_matrix = [
+header_list = ["int", "float", "str", "bool", "mix", "time"]
+value_matrix = [
     [0,   0.1,      "hoge", True,   0,      "2017-01-01 03:04:05+0900"],
     [2,   "-2.23",  "foo",  False,  None,   "2017-12-23 12:34:51+0900"],
     [3,   0,        "bar",  "true",  "inf", "2017-03-03 22:44:55+0900"],
     [-10, -9.9,     "",     "FALSE", "nan", "2017-01-01 00:00:00+0900"],
 ]
-writer.write_table()
 
+writer = pytablewriter.ExcelXlsxTableWriter()
+writer.open_workbook("sample.xlsx")
+writer.make_worksheet("example")
+writer.header_list = header_list
+writer.value_matrix = value_matrix
+writer.write_table()
+writer.close()
+
+writer = pytablewriter.ExcelXlsTableWriter()
+writer.open_workbook("sample.xls")
+writer.make_worksheet("example")
+writer.header_list = header_list
+writer.value_matrix = value_matrix
+writer.write_table()
 writer.close()
