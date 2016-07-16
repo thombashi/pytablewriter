d0b2b0aa3674fb6b85fd788e88a3a54f4cc22046
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-16 10:24:41 +0900

    Add an interface class and a base class of for Excel Workbook

--- pytablewriter/_excel_workbook.py
+++ pytablewriter/_excel_workbook.py
@@ -5,19 +5,48 @@
 """
 
 from __future__ import absolute_import
+import abc
 
+import six
 import xlsxwriter
 
 
-class ExcelWorkbookXlsx(object):
+@six.add_metaclass(abc.ABCMeta)
+class ExcelWorkbookInterface(object):
+
+    @abc.abstractproperty
+    def workbook(self):
+        pass
+
+    @abc.abstractproperty
+    def file_path(self):
+        pass
+
+    @abc.abstractmethod
+    def open(self, file_path):
+        pass
+
+    @abc.abstractmethod
+    def close(self):
+        pass
+
+
+class ExcelWorkbook(ExcelWorkbookInterface):
 
     @property
     def workbook(self):
-        return self.__workbook
+        return self._workbook
 
     @property
     def file_path(self):
-        return self.__file_path
+        return self._file_path
+
+    def _clear(self):
+        self._workbook = None
+        self._file_path = None
+
+
+class ExcelWorkbookXlsx(ExcelWorkbook):
 
     def __init__(self, file_path):
         self.open(file_path)
@@ -26,21 +55,17 @@ class ExcelWorkbookXlsx(object):
         self.close()
 
     def open(self, file_path):
-        self.__file_path = file_path
-        self.__workbook = xlsxwriter.Workbook(file_path)
+        self._file_path = file_path
+        self._workbook = xlsxwriter.Workbook(file_path)
 
     def close(self):
         if self.workbook is None:
             return
 
-        self.__workbook.close()
-        self.__clear()
+        self._workbook.close()
+        self._clear()
 
     def add_worksheet(self, worksheet_name):
-        worksheet = self.__workbook.add_worksheet(worksheet_name)
+        worksheet = self.workbook.add_worksheet(worksheet_name)
 
         return worksheet
-
-    def __clear(self):
-        self.__workbook = None
-        self.__file_path = None
