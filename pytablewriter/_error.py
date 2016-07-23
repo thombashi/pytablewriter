15bee4a152b76a23218c0608e6a804795be7c8e7
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-23 17:43:12 +0900

    Update docstring

--- pytablewriter/_error.py
+++ pytablewriter/_error.py
@@ -9,19 +9,20 @@ from __future__ import absolute_import
 
 class EmptyTableNameError(Exception):
     """
-    Raised when a table writer class of the |table_name| attribute is empty
-    and the writer is not permitted empty |table_name|.
+    Raised when a table writer class of the |table_name| attribute is null
+    and the class is not accepted null |table_name|.
     """
 
 
 class EmptyHeaderError(Exception):
     """
-    Raised when a table writer class of the |header_list| attribute is empty
-    and the writer is not permitted empty |header_list|.
+    Raised when a table writer class of the |header_list| attribute is null
+    and the class is not accepted null |header_list|.
     """
 
 
 class EmptyValueError(Exception):
     """
-    Raised when a table writer class of the |value_matrix| attribute is empty.
+    Raised when a table writer class of the |value_matrix| attribute is null
+    and the class is not accepted null |value_matrix|.
     """
