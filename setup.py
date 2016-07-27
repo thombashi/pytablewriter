102e27d01e9df3beb64765fd67ee483df0c2cde4
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-07-27 22:39:30 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="pytablewriter",
-    version="0.8.1",
+    version="0.8.2",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/pytablewriter",
