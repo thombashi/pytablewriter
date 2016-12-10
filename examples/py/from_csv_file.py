200147c646ef675e0ee9bafebabb3605346774af
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-10 23:43:55 +0900

    Initial commit

---
+++ examples/py/from_csv_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+import io
+import pytablewriter
+
+
+filename = "sample.csv"
+csv_data = u""""i","f","c","if","ifc","bool","inf","nan","mix_num","time"
+1,1.10,"aa",1.0,"1",True,Infinity,NaN,1,"2017-01-01 00:00:00+09:00"
+2,2.20,"bbb",2.2,"2.2",False,Infinity,NaN,Infinity,"2017-01-02 03:04:05+09:00"
+3,3.33,"cccc",-3.0,"ccc",True,Infinity,NaN,NaN,"2017-01-01 00:00:00+09:00"
+"""
+
+with io.open(filename, "w", encoding="utf8") as f:
+    f.write(csv_data)
+
+writer = pytablewriter.RstGridTableWriter()
+writer.from_csv(filename)
+writer.write_table()
