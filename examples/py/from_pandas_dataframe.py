b87b2c33f13280aa678720d5ee9907df119e1cb6
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-10 23:26:08 +0900

    Update an example

--- examples/py/from_pandas_dataframe.py
+++ examples/py/from_pandas_dataframe.py
@@ -5,19 +5,17 @@
 .. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
 """
 
-from __future__ import print_function
-
 import pandas as pd
 import pytablewriter
 from StringIO import StringIO
 
 
-csv = StringIO(u""""i","f","c","if","ifc","bool","inf","nan","mix_num","time"
+csv_data = StringIO(u""""i","f","c","if","ifc","bool","inf","nan","mix_num","time"
 1,1.10,"aa",1.0,"1",True,Infinity,NaN,1,"2017-01-01 00:00:00+09:00"
 2,2.20,"bbb",2.2,"2.2",False,Infinity,NaN,Infinity,"2017-01-02 03:04:05+09:00"
 3,3.33,"cccc",-3.0,"ccc",True,Infinity,NaN,NaN,"2017-01-01 00:00:00+09:00"
 """)
-df = pd.read_csv(csv, sep=',')
+df = pd.read_csv(csv_data, sep=',')
 
 writer = pytablewriter.MarkdownTableWriter()
 writer.from_dataframe(df)
