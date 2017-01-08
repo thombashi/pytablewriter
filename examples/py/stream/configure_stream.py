7fc931e14a4bd531e537e37d5913ca96ab1cb5a6
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-08 11:25:31 +0900

    Update examples

--- examples/py/stream/configure_stream.py
+++ examples/py/stream/configure_stream.py
@@ -8,11 +8,10 @@
 from __future__ import print_function
 
 import pytablewriter
+import six
 
 
-filename = "sample.rst"
-
-writer = pytablewriter.RstGridTableWriter()
+writer = pytablewriter.MarkdownTableWriter()
 writer.table_name = "zone"
 writer.header_list = ["zone_id", "country_code", "zone_name"]
 writer.value_matrix = [
@@ -23,9 +22,16 @@ writer.value_matrix = [
     ["5", "AI", "America/Anguilla"],
 ]
 
-with open(filename, "w") as f:
+# writer instance will write a table to stdout in default
+writer.write_table()
+
+# change stream to string buffer to get output as a string
+writer.stream = six.StringIO()
+writer.write_table()
+print()
+print(writer.stream.getvalue())
+
+# change output stream to a file
+with open("sample.md", "w") as f:
     writer.stream = f
     writer.write_table()
-
-with open(filename) as f:
-    print(f.read())
