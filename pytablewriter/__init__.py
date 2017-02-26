901ce429ca78d59896049ae5a109e44c857760b7
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-02-26 11:17:18 +0900

    Add logger interfaces

--- pytablewriter/__init__.py
+++ pytablewriter/__init__.py
@@ -28,8 +28,10 @@ from ._error import (
 )
 from ._factory import TableWriterFactory
 from ._function import dump_tabledata
-from ._logger import set_logger
-
+from ._logger import (
+    set_logger,
+    set_log_level,
+)
 from .writer._csv_writer import CsvTableWriter
 from .writer._excel_writer import (
     ExcelXlsTableWriter,
@@ -39,8 +41,8 @@ from .writer._html_writer import HtmlTableWriter
 from .writer._javascript_writer import JavaScriptTableWriter
 from .writer._json_writer import JsonTableWriter
 from .writer._ltsv_writer import LtsvTableWriter
-from .writer._mediawiki_writer import MediaWikiTableWriter
 from .writer._md_writer import MarkdownTableWriter
+from .writer._mediawiki_writer import MediaWikiTableWriter
 from .writer._null_writer import NullTableWriter
 from .writer._pandas_writer import PandasDataFrameWriter
 from .writer._python_code_writer import PythonCodeTableWriter
@@ -49,5 +51,5 @@ from .writer._rst_writer import (
     RstGridTableWriter,
     RstSimpleTableWriter
 )
-from .writer._tsv_writer import TsvTableWriter
 from .writer._toml_writer import TomlTableWriter
+from .writer._tsv_writer import TsvTableWriter
--- pytablewriter/_logger.py
+++ pytablewriter/_logger.py
@@ -14,21 +14,28 @@ logger = logbook.Logger("pytablewriter")
 logger.disable()
 
 
-def set_logger(log_level):
+def set_logger(is_enable):
+    if is_enable:
+        logger.enable()
+    else:
+        logger.disable()
+
+
+def set_log_level(log_level):
     """
-    Set logging level of this module. The module using
+    Set logging level of this module. Using
     `logbook <http://logbook.readthedocs.io/en/stable/>`__ module for logging.
 
     :param int log_level:
-        One of the log level of the
+        One of the log level of
         `logbook <http://logbook.readthedocs.io/en/stable/api/base.html>`__.
-        Disabled logging if the ``log_level`` is ``logbook.NOTSET``.
+        Disabled logging if ``log_level`` is ``logbook.NOTSET``.
     """
 
     if log_level == logbook.NOTSET:
-        logger.disable()
+        set_logger(is_enable=False)
     else:
-        logger.enable()
+        set_logger(is_enable=True)
         logger.level = log_level
 
 
