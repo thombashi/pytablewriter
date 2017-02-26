6c0aa0df5899929928ca4827fc82efb822543f54
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-02-26 10:57:42 +0900

    Update docstring

--- pytablewriter/_error.py
+++ pytablewriter/_error.py
@@ -13,33 +13,33 @@ class NotSupportedError(Exception):
 
 class EmptyTableNameError(Exception):
     """
-    Raised when a table writer class of the |table_name| attribute is null
+    Exception raised when a table writer class of the |table_name| attribute is null
     and the class is not accepted null |table_name|.
     """
 
 
 class EmptyHeaderError(Exception):
     """
-    Raised when a table writer class of the |header_list| attribute is null
+    Exception raised when a table writer class of the |header_list| attribute is null
     and the class is not accepted null |header_list|.
     """
 
 
 class EmptyValueError(Exception):
     """
-    Raised when a table writer class of the |value_matrix| attribute is null
+    Exception raised when a table writer class of the |value_matrix| attribute is null
     and the class is not accepted null |value_matrix|.
     """
 
 
 class EmptyTableDataError(Exception):
     """
-    Raised when a table writer class of the |header_list| and |value_matrix|
+    Exception raised when a table writer class of the |header_list| and |value_matrix|
     attributes are null
     """
 
 
 class WriterNotFoundError(Exception):
     """
-    Raised when appropriate loader writer found.
+    Exception raised when appropriate loader writer found.
     """
