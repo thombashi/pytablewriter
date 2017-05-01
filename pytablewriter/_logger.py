37e7ff7ea441124b459b62a06e8cc7cb7843254a
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-05-01 17:49:14 +0900

    Add information to log message of write tables

--- pytablewriter/_logger.py
+++ pytablewriter/_logger.py
@@ -47,16 +47,24 @@ class WriterLogger(object):
             "created WriterLogger: format={}".format(writer.format_name))
 
     def logging_write(self):
-        log_message = "write table: type={:s}, table-name='{}', header={}".format(
-            self.__writer.format_name, self.__writer.table_name,
-            self.__writer.header_list)
+        log_entry_list = [
+            "type={:s}".format(self.__writer.format_name),
+            "table-name='{}'".format(self.__writer.table_name),
+            "header={}".format(self.__writer.header_list),
+        ]
 
         try:
-            log_message += ", type-hint={}".format([
-                type_hint(None).typename
-                for type_hint in self.__writer.type_hint_list
-            ])
+            log_entry_list.append(
+                "rows={}".format(len(self.__writer.value_matrix)))
         except (TypeError, AttributeError):
             pass
 
-        logger.debug(log_message)
+        try:
+            log_entry_list.append("type-hint={}".format([
+                type_hint(None).typename
+                for type_hint in self.__writer.type_hint_list
+            ]))
+        except (TypeError, AttributeError):
+            pass
+
+        logger.debug("write table: " + ", ".join(log_entry_list))
