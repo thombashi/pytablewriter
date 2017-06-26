6f80ccb9c620d132a283e9d489cbd3c1b979122a
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-06-26 23:07:09 +0900

    Update elasticsearch example

--- examples/py/elasticsearch_index.py
+++ examples/py/elasticsearch_index.py
@@ -1,41 +1,58 @@
+#!/usr/bin/env python
 # encoding: utf-8
 
 """
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
-from __future__ import unicode_literals
-
 import datetime
-
-import logbook
+import json
 
 from elasticsearch import Elasticsearch
+
 import pytablewriter as ptw
 
 
-logbook.StderrHandler(
-    level=logbook.DEBUG,
-    format_string="[{record.level_name}] {record.channel}: {record.message}"
-).push_application()
-
+es = Elasticsearch(hosts="localhost:9200")
+index_name = "es_writer_example"
 
 writer = ptw.ElasticsearchWriter()
-
-writer.stream = Elasticsearch(hosts="localhost:9200")
-writer.table_name = "es_mappings"
+writer.stream = es
+writer.table_name = index_name
 writer.header_list = [
     "str", "byte", "short", "int", "long", "float", "date", "bool", "ip",
 ]
 writer.value_matrix = [
     [
         "abc", 100, 10000, 2000000000, 200000000000, 0.1,
-        datetime.datetime(2017, 1, 3, 4, 5, 6), True, "127.0.0.1",
+        datetime.datetime(2017, 1, 2, 3, 4, 5), True, "127.0.0.1",
     ],
     [
         "def", -10, -1000, -200000000, -20000000000, 100.1,
-        datetime.datetime(2017, 1, 3, 4, 5, 6), False, "::1",
+        datetime.datetime(2017, 6, 5, 4, 5, 2), False, "::1",
     ],
 ]
 
+# delete existing index ---
+es.indices.delete(index=index_name, ignore=404)
+
+# create an index and put data ---
 writer.write_table()
+
+# display the result ---
+es.indices.refresh(index=index_name)
+
+print("----- mappings -----")
+response = es.indices.get_mapping(index=index_name, doc_type="table")
+print("{}\n".format(json.dumps(response, indent=4)))
+
+print("----- documents -----")
+response = es.search(
+    index=index_name,
+    doc_type="table",
+    body={
+        "query": {"match_all": {}}
+    }
+)
+for hit in response["hits"]["hits"]:
+    print(json.dumps(hit["_source"], indent=4))
