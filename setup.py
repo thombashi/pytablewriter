118886663429846e527aada7789664be37cf4437
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-07-31 17:15:31 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -31,28 +31,19 @@ with io.open(
 with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
     install_requires = [line.strip() for line in f if line.strip()]
 
-if any([
-    sys.version_info.major < 3,
-    sys.version_info.major == 3 and sys.version_info.minor < 4,
-]):
-    install_requires.append("enum34")
-
-if any([
-    sys.version_info.major < 3,
-    sys.version_info.major == 3 and sys.version_info.minor < 3,
-]):
-    install_requires.append("ipaddress")
-
 with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
-    tests_require = [line.strip() for line in f if line.strip()]
+    tests_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
+    docs_requires = [line.strip() for line in f if line.strip()]
 
 setuptools.setup(
     name="pytablewriter",
     version="0.23.0",
+    url="https://github.com/thombashi/pytablewriter",
+
     author="Tsuyoshi Hombashi",
     author_email="tsuyoshi.hombashi@gmail.com",
-    url="https://github.com/thombashi/pytablewriter",
-    license="MIT License",
     description=summary,
     include_package_data=True,
     install_requires=install_requires,
@@ -61,10 +52,17 @@ setuptools.setup(
         "Markdown", "MediaWiki", "HTML", "pandas", "reStructuredText",
         "SQLite", "TSV", "TOML",
     ],
+    license="MIT License",
     long_description=long_description,
     packages=setuptools.find_packages(exclude=["test*"]),
+
     setup_requires=pytest_runner,
-    tests_require=tests_require,
+    tests_require=tests_requires,
+    extras_require={
+        "test": tests_requires,
+        "docs": docs_requires,
+    },
+
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
