5b24e1e4288d4a3fd03ebca4b845f30e30e76c0b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-01 10:19:37 +0900

    Add log level validation

--- pytablewriter/_logger.py
+++ pytablewriter/_logger.py
@@ -41,7 +41,11 @@ def set_log_level(log_level):
         One of the log level of
         `logbook <http://logbook.readthedocs.io/en/stable/api/base.html>`__.
         Disabled logging if ``log_level`` is ``logbook.NOTSET``.
+    :raises LookupError: If ``log_level`` is an invalid value.
     """
+
+    # validate log level
+    logbook.get_level_name(log_level)
 
     if log_level == logger.level:
         return
