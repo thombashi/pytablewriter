ee45b4fae08746c695e32e8a0f59da12f8b1d913
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-01 10:22:57 +0900

    Modify logging setup to avoid setup recursion

--- pytablewriter/_logger.py
+++ pytablewriter/_logger.py
@@ -54,8 +54,8 @@ def set_log_level(log_level):
         set_logger(is_enable=False)
     else:
         set_logger(is_enable=True)
-        logger.level = log_level
 
+    logger.level = log_level
     dataproperty.set_log_level(log_level)
     pytablereader.set_log_level(log_level)
     simplesqlite.set_log_level(log_level)
