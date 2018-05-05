8df534e9c44cb3df7b0c60ef1ed3a18175a166f4
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-05 22:43:36 +0900

    Add python_requires

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setuptools.setup(
         "Tracker": "{:s}/issues".format(REPOSITORY_URL),
     },
 
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
     install_requires=setuptools_require + install_requires,
     setup_requires=setuptools_require + pytest_runner_require,
     tests_require=tests_requires,
