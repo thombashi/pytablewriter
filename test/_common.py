6c431129e64380754296b646e5e063521980b8da
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-05 15:34:18 +0900

    Add a test helper function

---
+++ test/_common.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+'''
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+'''
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+def print_result(expected, actual):
+    print("[expected]\n{}\n".format(expected))
+    print("[actual]\n{}\n".format(actual))
