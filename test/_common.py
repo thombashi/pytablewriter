7ee2ea4f3034a6bfc4bcfb78b7c2cc1e3887fb55
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-06-09 09:35:25 +0900

    Add an argument to print stderr of test results

--- test/_common.py
+++ test/_common.py
@@ -6,7 +6,12 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import sys
 
-def print_test_result(expected, actual):
+
+def print_test_result(expected, actual, error=None):
     print("[expected]\n{}\n".format(expected))
     print("[actual]\n{}\n".format(actual))
+
+    if error:
+        print(error, file=sys.stderr)
