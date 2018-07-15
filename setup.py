5714a11b0eb2ac7277a3bbf84fca27de610f65af
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-07-15 23:40:37 +0900

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -101,6 +101,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
