73f475147cea8f36f3cf51bbe915f10e8f0c5370
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-07-23 00:37:36 +0900

    Update examples

--- examples/py/from_pandas_dataframe.py
+++ examples/py/from_pandas_dataframe.py
@@ -13,13 +13,12 @@ import six
 import pandas as pd
 
 
-csv_data = six.StringIO(dedent("""\
+df = pd.read_csv(six.StringIO(dedent("""\
     "i","f","c","if","ifc","bool","inf","nan","mix_num","time"
     1,1.10,"aa",1.0,"1",True,Infinity,NaN,1,"2017-01-01 00:00:00+09:00"
-    2,2.20,"bbb",2.2,"2.2",False,Infinity,NaN,Infinity,"2017-01-02 03:04:05+09:00"
-    3,3.33,"cccc",-3.0,"ccc",True,Infinity,NaN,NaN,"2017-01-01 00:00:00+09:00"
-    """))
-df = pd.read_csv(csv_data, sep=',')
+    22,2.20,"bbb",2.2,"2.2",False,Infinity,NaN,Infinity,"2017-01-02 03:04:05+09:00"
+    333,3.33,"cccc",-3.0,"ccc",True,Infinity,NaN,NaN,"2017-01-01 00:00:00+09:00"
+    """)), sep=',')
 
 writer = pytablewriter.MarkdownTableWriter()
 writer.from_dataframe(df)
