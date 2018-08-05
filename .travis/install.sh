ca8abbfd61c9f4d5d0f4ba456b408f88cf7d78c7
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-08-05 17:41:41 +0900

    Fix Python3 installation for macOS CI tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ "$TRAVIS_OS_NAME" = "osx" ] && ! python3; then
-    # Install Python3 on osx
-    brew upgrade python
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    if ! type python3 > /dev/null 2>&1; then
+        # Install Python3 on osx
+        brew upgrade python
+    fi
+
     pip3 install setuptools --upgrade
     pip3 install .[test]
 else
