bc0bc6451dd201f3e9590b866865ec7e02bd28dd
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-07 00:17:16 +0900

    Apply isort

--- pytablewriter/writer/__init__.py
+++ pytablewriter/writer/__init__.py
@@ -3,27 +3,25 @@
 from __future__ import absolute_import
 
 from ._elasticsearch import ElasticsearchWriter
+from ._null import NullTableWriter
+from .binary import ExcelXlsTableWriter, ExcelXlsxTableWriter, SqliteTableWriter
 from .text import (
-    JsonTableWriter,
     CsvTableWriter,
     HtmlTableWriter,
     JsonLinesTableWriter,
+    JsonTableWriter,
     LatexMatrixWriter,
     LatexTableWriter,
     LtsvTableWriter,
     MarkdownTableWriter,
+    MediaWikiTableWriter,
     RstCsvTableWriter,
     RstGridTableWriter,
     RstSimpleTableWriter,
     SpaceAlignedTableWriter,
     TomlTableWriter,
     TsvTableWriter,
-    MediaWikiTableWriter,
 )
-from ._null import NullTableWriter
-from .binary import ExcelXlsTableWriter, ExcelXlsxTableWriter, SqliteTableWriter
-
-
 from .text.sourcecode import (
     JavaScriptTableWriter,
     NumpyTableWriter,
