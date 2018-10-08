fbcdcab18f11474110b9254868cd6fb988dab3f0
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-08 18:59:01 +0900

    Modify a package installation for CI tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,8 +7,8 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     fi
 
     pip3 install setuptools --upgrade
-    pip3 install .[test]
+    pip3 install .[es6,test]
 else
     pip install setuptools --upgrade
-    pip install .[test]
+    pip install .[es6,test]
 fi
