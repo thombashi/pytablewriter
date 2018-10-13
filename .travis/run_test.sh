d4d165f93290a390c73e0191b841fd9bf7586b7b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-13 21:10:23 +0900

    Modify Travis CI that obtain coverage

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
-
-tox
-
-if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$pyver" = "36" ]; then
-    python setup.py test --addopts "-v --cov pytablewriter --cov-report term-missing"
+if [ "$TOXENV" != "cov" ] ; then
+    tox
 fi
