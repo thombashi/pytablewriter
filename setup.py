044c19133e95414d9eaa2a713ef63592210f9729
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-01-13 12:58:01 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -57,9 +57,9 @@ pytest_runner_require = ["pytest-runner"] if need_pytest() else []
 
 excel_requires = ["xlwt", "XlsxWriter>=1.1.2,<2.0.0"]
 es6_requires = ["elasticsearch>=6.2.0,<7.0.0"]
-from_requires = ["pytablereader>=0.22.7,<1.0.0"]
-sqlite_requires = ["SimpleSQLite>=0.33.6,<1.0.0"]
+from_requires = ["pytablereader>=0.23.0,<1.0.0"]
 logging_requires = ["Logbook>=1.1.0,<2.0.0"]
+sqlite_requires = ["SimpleSQLite>=0.34.0,<1.0.0"]
 toml_requires = ["toml>=0.9.4,<1.0.0"]
 optional_requires = ["simplejson>=3.16,<4.0"]
 all_requires = (
