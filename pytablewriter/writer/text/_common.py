c14e50a09687f057155c8613324efc4c1e1c89e5
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-01-27 23:25:35 +0900

    Add a common file for text writers

---
+++ pytablewriter/writer/text/_common.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+import sys
+
+
+def bool_to_str(value):
+    if value is True:
+        return "true"
+    if value is False:
+        return "false"
+
+    return value
