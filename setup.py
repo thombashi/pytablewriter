6f1e996a40e2e68f48249e8710ae8aa6be228997
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-02-11 09:20:47 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -55,12 +55,12 @@ with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
 setuptools_require = ["setuptools>=38.3.0"]
 pytest_runner_require = ["pytest-runner"] if need_pytest() else []
 
-excel_requires = ["xlwt", "XlsxWriter>=1.1.2,<2.0.0"]
+excel_requires = ["xlwt", "XlsxWriter>=1.1.4,<2.0.0"]
 es6_requires = ["elasticsearch>=6.2.0,<7.0.0"]
 from_requires = ["pytablereader>=0.24.1,<1.0.0"]
 html_requires = ["dominate>=2.3.5,<3.0.0"]
 logging_requires = ["Logbook>=1.1.0,<2.0.0"]
-sqlite_requires = ["SimpleSQLite>=0.38.0,<1.0.0"]
+sqlite_requires = ["SimpleSQLite>=0.39.0,<1.0.0"]
 toml_requires = ["toml>=0.9.4,<1.0.0"]
 optional_requires = ["simplejson>=3.16,<4.0"]
 all_requires = (
