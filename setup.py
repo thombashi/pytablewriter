bcdb402d67a0ac38d79895124ce0260ef09ab2b5
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-05-11 21:29:40 +0900

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -129,6 +129,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
