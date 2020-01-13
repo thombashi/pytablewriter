af879ee38bf8a4338668667306472385d8cc2493
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-13 13:31:20 +0900

    Update classifiers

--- setup.py
+++ setup.py
@@ -131,6 +131,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
