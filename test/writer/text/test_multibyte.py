fab27cca81852887e87be9b8856b6cc5e19320e6
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-08-30 23:09:48 +0900

    Refactor a test case

--- test/writer/text/test_multibyte.py
+++ test/writer/text/test_multibyte.py
@@ -10,26 +10,20 @@ import pytablewriter as ptw
 class Test_CsvTableWriter_write_table:
     @pytest.mark.parametrize(
         ["format_name"],
-        [
-            [format_name]
-            for format_name in ptw.TableWriterFactory.get_format_names()
-            if format_name not in ["null", "elasticsearch", "excel", "sqlite"]
-        ],
+        [[tblfmt.names[0]] for tblfmt in ptw.TableFormat.find_all_attr(ptw.FormatAttr.TEXT)],
     )
-    def test_smoke_multi_byte(self, capsys, format_name):
-        writer = ptw.TableWriterFactory.create_from_format_name(format_name)
-        writer.table_name = "生成に関するパターン"
-        writer.headers = ["パターン名", "概要", "GoF", "Code Complete[1]"]
-        writer.value_matrix = [
-            ["Abstract Factory", "関連する一連のインスタンスを状況に応じて、適切に生成する方法を提供する。", "Yes", "Yes"],
-            ["Builder", "複合化されたインスタンスの生成過程を隠蔽する。", "Yes", "No"],
-            ["Factory Method", "実際に生成されるインスタンスに依存しない、インスタンスの生成方法を提供する。", "Yes", "Yes"],
-            ["Prototype", "同様のインスタンスを生成するために、原型のインスタンスを複製する。", "Yes", "No"],
-            ["Singleton", "あるクラスについて、インスタンスが単一であることを保証する。", "Yes", "Yes"],
-        ]
+    def test_smoke_multi_byte(self, format_name):
+        writer = ptw.TableWriterFactory.create_from_format_name(
+            format_name,
+            table_name="生成に関するパターン",
+            headers=["パターン名", "概要", "GoF", "Code Complete[1]"],
+            value_matrix=[
+                ["Abstract Factory", "関連する一連のインスタンスを状況に応じて、適切に生成する方法を提供する。", "Yes", "Yes"],
+                ["Builder", "複合化されたインスタンスの生成過程を隠蔽する。", "Yes", "No"],
+                ["Factory Method", "実際に生成されるインスタンスに依存しない、インスタンスの生成方法を提供する。", "Yes", "Yes"],
+                ["Prototype", "同様のインスタンスを生成するために、原型のインスタンスを複製する。", "Yes", "No"],
+                ["Singleton", "あるクラスについて、インスタンスが単一であることを保証する。", "Yes", "Yes"],
+            ],
+        )
 
-        writer.write_table()
-
-        out, _err = capsys.readouterr()
-
-        assert len(out) > 100
+        assert len(writer.dumps()) > 100
