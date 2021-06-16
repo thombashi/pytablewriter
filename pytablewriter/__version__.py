855e6295611fa823ade9d1bb5c74cbad625fd623
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-06-16 22:21:21 +0900

    Bump version

--- pytablewriter/__version__.py
+++ pytablewriter/__version__.py
@@ -1,6 +1,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = f"Copyright 2016, {__author__}"
 __license__ = "MIT License"
-__version__ = "0.60.0"
+__version__ = "0.61.0"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
