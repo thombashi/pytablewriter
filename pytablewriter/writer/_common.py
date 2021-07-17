8991037e998e75f2bdd8b9ab1f17a1602a439c94
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-07-17 15:47:33 +0900

    Remove an unused class

--- pytablewriter/writer/_common.py
+++ pytablewriter/writer/_common.py
@@ -10,11 +10,3 @@ import_error_msg_template = dedent(
     you can install the dependencies with 'pip install pytablewriter[{0}]'
     """
 )
-
-
-class Theme:
-    class Key:
-        STYLE_FILTER = "style_filter"
-        COL_SEPARATOR_STYLE_FILTER = "col_separator_style_filter"
-
-    KNOWN_PLUGINS = ("altrow",)
