992692ddda258045001b0ae7a3e23719cb427cd1
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-06-11 09:13:03 +0900

    Change the Cell class to an immutable data class

--- pytablewriter/style/_cell.py
+++ pytablewriter/style/_cell.py
@@ -1,37 +1,26 @@
+from dataclasses import dataclass
 from typing import Any
 
 from ._style import Style
 
 
-# @dataclass  # starting PYthon 3.7
+@dataclass(frozen=True)
 class Cell:
     """
-    .. py:attribute:: row
-        :type: int
-
-        row index.
-
-    .. py:attribute:: col
-        :type: int
-
-        column index.
-
-    .. py:attribute:: value
-        :type: Any
-
-        data for the cell.
-
-    .. py:attribute:: default_style
-        :type: pytablewriter.style.Style
-
-        default |Style| for the cell.
+    A data class representing a cell in a table.
     """
 
-    def __init__(self, row: int, col: int, value: Any, default_style: Style):
-        self.row = row
-        self.col = col
-        self.value = value
-        self.default_style = default_style
+    row: int
+    """row index. ``-1`` means that the table header row."""
+
+    col: int
+    """column index."""
+
+    value: Any
+    """data for the cell."""
+
+    default_style: Style
+    """default |Style| for the cell."""
 
     def is_header_row(self) -> bool:
         """
