921d552c6343088a3085850f262521053f43510d
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-06-25 18:54:10 +0900

    Bump Development Status

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setuptools.setup(
         "yaml": yaml_requires,
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
