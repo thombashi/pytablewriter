061084932defd41e9498f7259e434505ca7d6cd2
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-09-23 22:11:26 +0900

    Update classifiers

--- setup.py
+++ setup.py
@@ -151,6 +151,7 @@ setuptools.setup(
         "Topic :: Text Processing :: Markup :: LaTeX",
         "Topic :: Text Processing :: Markup :: Markdown",
         "Topic :: Text Processing :: Markup :: reStructuredText",
+        "Typing :: Typed",
     ],
     cmdclass=get_release_command_class(),
     zip_safe=False,
