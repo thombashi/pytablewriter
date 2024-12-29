8307b8abddb5007c1e592d3163f5218c4150a180
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-12-29 13:23:06 +0900

    Fix typo in project_urls key from "Changlog" to "Changelog"
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["test*"]),
     package_data={MODULE_NAME: ["py.typed"]},
     project_urls={
-        "Changlog": f"{REPOSITORY_URL:s}/releases",
+        "Changelog": f"{REPOSITORY_URL:s}/releases",
         "Documentation": f"https://{MODULE_NAME:s}.rtfd.io/",
         "Funding": "https://github.com/sponsors/thombashi",
         "Source": REPOSITORY_URL,
