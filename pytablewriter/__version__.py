91273b9d5ec95b371896fc43f9752ce16e07c622
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-01-02 00:33:44 +0900

    Bump version to 1.2.1
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- pytablewriter/__version__.py
+++ pytablewriter/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2016-2025, {__author__}"
 __license__: Final = "MIT License"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
